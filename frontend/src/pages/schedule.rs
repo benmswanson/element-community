90137b8569f8b8e17c836401be1b47c763b193bc
Ben Swanson <benjaminmswanson@gmail.com>
2026-04-21 11:35:26 -0400

    Embed MindBody schedule widget on schedule page

--- frontend/src/pages/schedule.rs
+++ frontend/src/pages/schedule.rs
@@ -1,17 +1,19 @@
 use yew::prelude::*;
 
-use crate::components::section::Section;
-
 #[function_component(Schedule)]
 pub fn schedule() -> Html {
     html! {
-        <>
-            <Section
-                eyebrow="Coming Soon"
-                title="Calendar coming soon."
-                description="We're building an integrated calendar for class schedules and community events. Check back soon."
-            >
-            </Section>
-        </>
+        <div class="shell-container schedule-embed">
+            <div class="mindbody-widget" data-widget-type="Schedules" data-widget-id="ea342103127">
+                <iframe
+                    id="bw-widget-iframe-f1007"
+                    class="bw-widget-styles"
+                    src="https://go.mindbodyonline.com/book/widgets/schedules/view/ea342103127/schedule"
+                    allow="payment"
+                    scrolling="no"
+                    style="overflow: hidden; width: 100%; height: 896px; border: none;"
+                />
+            </div>
+        </div>
     }
 }
