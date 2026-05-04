e58239d458d191fec6af280d6f0007a7fbdaf8e7
Ben Swanson <benjaminmswanson@gmail.com>
2026-05-04 16:28:41 -0400

    Book club: cover with top position to show title/author

--- frontend/src/pages/tv.rs
+++ frontend/src/pages/tv.rs
@@ -47,8 +47,8 @@ const TV_CARDS: [TvCard; 3] = [
         btn_label: "RSVP",
         rotation: "-2deg",
         pin_color: "pin-blue",
-        img_position: "center",
-        img_fit: "contain",
+        img_position: "top",
+        img_fit: "cover",
     },
 ];
 
